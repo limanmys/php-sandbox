30257dfa201a4a51683d1df4aac9aa7ae0f11213
mertcelen <mcelen94@gmail.com>
2019-07-26 09:06:08 +0300

    Fix for JSTree

--- views/folder.blade.php
+++ views/folder.blade.php
@@ -1,9 +1,10 @@
 @foreach($files as $key => $file)
+    @php($random = $key. '*LIMAN*'.\Illuminate\Support\Str::random(10) . '*LIMAN*')
     @if(is_array($file))
         @if(strpos($key,"="))
-            { "text" : "{{explode("=",$key)[1]}}", "children" : [@include('folder',["files" => $file])], "id" : "{{$key}}"},
+            { "text" : "{{explode("=",$key)[1]}}", "children" : [@include('folder',["files" => $file])], "id" : "{{$random}}"},
         @else
-            { "text" : "{{$key}}", "children" : [@include('folder',["files" => $file])],"id" : "{{$key}}"},
+            { "text" : "{{$key}}", "children" : [@include('folder',["files" => $file])],"id" : "{{$random}}"},
         @endif
         
     @else
