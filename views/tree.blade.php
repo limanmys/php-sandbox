8c0d0cd33979a2f6c9efc12c290827846a7b6882
mertcelen <mcelen94@gmail.com>
2020-02-14 16:58:10 +0300

    Changed tree expand layout

--- views/tree.blade.php
+++ views/tree.blade.php
@@ -1,8 +1,17 @@
 @php($random = (isset($id)? $id : "a" . str_random(20)))
 <input class="form-control" type="search" onchange="search{{$random}}(this)"/><br>
-<button class="btn btn-primary" onclick="toogle{{$random}}()"><i id="icon{{$random}}" class="fa fa-compress"></i></button><br>
+
+<div class="row">
+    <div class="col-md-1">
+    <button class="btn btn-warning" onclick="toogle{{$random}}()"><i id="icon{{$random}}" class="fa fa-caret-down"></i></button><br>
+
+    </div>
+    <div class="col-md-10">
+    <div id="{{$random}}" style="overflow-x: auto; max-height: 450px; overflow-y: auto;"></div>
+
+    </div>
+</div>
 <br>
-<div id="{{$random}}" style="overflow-x: auto; max-height: 450px; overflow-y: auto;"></div>
 <script>
     $('#{{$random}}').jstree({
         "plugins": [
@@ -50,7 +59,7 @@
             $('#{{$random}}').jstree('close_all');
         }
         open{{$random}} = !open{{$random}};
-        $("#icon{{$random}}").toggleClass('fa-expand').toggleClass('fa-compress');
+        $("#icon{{$random}}").toggleClass('fa-caret-down').toggleClass('fa-caret-up');
     }
 
     @isset($menu)
