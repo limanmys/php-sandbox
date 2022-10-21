630274ce51b40787b557962aa8372593359cc8f6
dogukanoksuz <me@dogukan.dev>
2022-10-21 10:58:45 +0000

    chore: Code refactoring

--- index.php
+++ index.php
@@ -2,21 +2,6 @@
 ob_start();
 require_once(__DIR__ . "/vendor/autoload.php");
 
-function customErrorHandler($exception, $err_str=null) {
-    if($err_str){
-        if($exception == 8){
-            return;
-        }
-        $message = __($err_str);
-    }else{
-        $message = __($exception->getMessage());
-    }
-    abort($message, 201);
-}
-
-set_exception_handler('customErrorHandler');
-set_error_handler('customErrorHandler');
-
 use Jenssegers\Blade\Blade;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
@@ -25,12 +10,30 @@ use Illuminate\Support\Str;
 use mervick\aesEverywhere\AES256;
 use Liman\Toolkit\RemoteTask\TaskManager;
 
+function customErrorHandler($exception, $err_str = null)
+{
+    if ($err_str) {
+        if ($exception == 8) {
+            return;
+        }
+
+        $message = __($err_str);
+    } else {
+        $message = __($exception->getMessage());
+    }
+
+    abort($message, 201);
+}
+
+set_exception_handler('customErrorHandler');
+set_error_handler('customErrorHandler');
+
 $decrypted = AES256::decrypt($argv[2], shell_exec('cat ' . $argv[1]));
 
-$limanData = json_decode($decrypted, true, 512);
+$limanData = json_decode((string) $decrypted, true, 512);
 
 foreach ($limanData as $key => $item) {
-    if(is_string($item)){
+    if (is_string($item)) {
         @$json = json_decode($item, true);
         $limanData[$key] = (json_last_error() == JSON_ERROR_NONE) ? $json : $item;
     }
@@ -54,7 +57,7 @@ function extensionDb($target, $set = null)
     if ($set) {
         $limanData["settings"][$target] = $set;
 
-        return renderEngineRequest('','setExtensionDb',[
+        return renderEngineRequest('', 'setExtensionDb', [
             "target" => $target,
             "new_param" => $set
         ]);
@@ -63,13 +66,13 @@ function extensionDb($target, $set = null)
     if (isset($limanData["settings"][$target])) {
         return $limanData["settings"][$target];
     }
+
     return "";
 }
 
 function extension()
 {
-    $json = json_decode(file_get_contents(getPath("db.json")), true);
-    return $json;
+    return json_decode(file_get_contents(getPath("db.json")), true);
 }
 
 function server()
@@ -77,43 +80,43 @@ function server()
     global $limanData;
     return (object) $limanData["server"];
 }
- 
+
 function user()
 {
     global $limanData;
     return (object) $limanData["user"];
 }
 
-$cachedLMNTranslations = [];
+$cachedTranslations = [];
 
 function __($str, $attributes = [])
 {
-    global $limanData, $cachedLMNTranslations;
-    if ($cachedLMNTranslations === null){
+    global $limanData, $cachedTranslations;
+    if ($cachedTranslations === null) {
         return $str;
-    } 
+    }
 
-    if ($cachedLMNTranslations == []){
+    if ($cachedTranslations == []) {
         $translationFiles = [
             [
-                "folder" => dirname(dirname($limanData["functionsPath"])) . "/lang",
+                "folder" => dirname(dirname((string) $limanData["functionsPath"])) . "/lang",
             ],
             [
                 "folder" => __DIR__ . "/lang",
             ]
         ];
-        foreach ($translationFiles as $translationFile){
+        foreach ($translationFiles as $translationFile) {
             $file = $translationFile['folder'] . "/" . $limanData["locale"] . ".json";
             if (is_dir($translationFile['folder']) && is_file($file)) {
-                $cachedLMNTranslations = array_merge(
-                    $cachedLMNTranslations,
+                $cachedTranslations = array_merge(
+                    $cachedTranslations,
                     json_decode(file_get_contents($file), true)
                 );
             }
         }
     }
-    
-    return makeReplacements((array_key_exists($str, $cachedLMNTranslations)) ? $cachedLMNTranslations[$str] : $str, $attributes);
+
+    return makeReplacements((array_key_exists($str, $cachedTranslations)) ? $cachedTranslations[$str] : $str, $attributes);
 }
 
 function makeReplacements($line, array $replace)
@@ -126,9 +129,9 @@ function makeReplacements($line, array $replace)
 
     foreach ($replace as $key => $value) {
         $line = str_replace(
-            [':'.$key, ':'.Str::upper($key), ':'.Str::ucfirst($key)],
+            [':' . $key, ':' . Str::upper($key), ':' . Str::ucfirst($key)],
             [$value, Str::upper($value), Str::ucfirst($value)],
-            $line
+            (string) $line
         );
     }
 
@@ -137,7 +140,7 @@ function makeReplacements($line, array $replace)
 
 function sortReplacements(array $replace)
 {
-    return (new Collection($replace))->sortBy(function ($value, $key) {
+    return (new Collection($replace))->sortBy(static function ($value, $key) {
         return mb_strlen($key) * -1;
     })->all();
 }
@@ -148,11 +151,12 @@ function request($target = null)
     $tempRequest = $limanData["requestData"];
     if ($target) {
         if (isset($tempRequest[$target])) {
-            return html_entity_decode($tempRequest[$target]);
+            return html_entity_decode((string) $tempRequest[$target]);
         } else {
             return "";
         }
     }
+
     return $tempRequest;
 }
 
@@ -171,7 +175,7 @@ function respond($message, $code = "200")
 {
     return json_encode([
         "message" => $message,
-        "status" => intval($code)
+        "status" => (int) $code
     ]);
 }
 
@@ -179,15 +183,16 @@ function abort($message, $code = "200")
 {
     global $limanData;
     ob_clean();
-    if(!boolval($limanData["ajax"])){
+    if (!(bool) $limanData["ajax"]) {
         echo view('alert', [
-            "type" => intval($code) == 200 ? "success" : "danger",
-            "title" => intval($code) == 200 ? __("Başarılı") : __("Hata"),
+            "type" => (int) $code == 200 ? "success" : "danger",
+            "title" => (int) $code == 200 ? __("Başarılı") : __("Hata"),
             "message" => $message
         ]);
-    }else{
+    } else {
         echo respond($message, $code);
     }
+
     exit();
 }
 
@@ -198,9 +203,10 @@ function navigate($name, $params = [])
     if ($params != []) {
         $args = '?&';
         foreach ($params as $key => $param) {
-            $args = $args . "&$key=$param";
+            $args .= sprintf('&%s=%s', $key, $param);
         }
     }
+
     return $limanData["navigationRoute"] . '/' . $name . $args;
 }
 
@@ -208,30 +214,32 @@ function view($name, $params = [])
 {
     global $limanData;
     $path = "/tmp/" . $limanData["extension"]["id"];
-    if(!is_dir($path)){
+    if (!is_dir($path)) {
         mkdir($path);
     }
-    $blade = new Blade([dirname($limanData["functionsPath"]), __DIR__ . "/views/"], $path);
+
+    $blade = new Blade([dirname((string) $limanData["functionsPath"]), __DIR__ . "/views/"], $path);
     return $blade->render($name, $params);
 }
 
-function limanInternalRequest($url,$data, $server_id = null,$extension_id = null)
+function limanInternalRequest($url, $data, $server_id = null, $extension_id = null)
 {
     global $limanData;
     $client = new Client([
         'verify' => false
     ]);
     $extraParams = [];
-    foreach($data as $key => $value){
+    foreach ($data as $key => $value) {
         $extraParams[] = [
             "name" => $key,
             "contents" => $value
         ];
     }
+
     $server_id = ($server_id) ? $server_id : server()->id;
     $extension_id = ($extension_id) ? $extension_id : $limanData["extension"]["id"];
     try {
-        $response = $client->request('POST', "https://127.0.0.1/lmn/private/$url", [
+        $response = $client->request('POST', sprintf('https://127.0.0.1/lmn/private/%s', $url), [
             'headers' => [
                 'Accept'     => 'application/json',
             ],
@@ -251,53 +259,54 @@ function limanInternalRequest($url,$data, $server_id = null,$extension_id = null
             ]),
         ]);
         return $response->getBody()->getContents();
-    } catch (GuzzleException $exception) {
-        if($exception->getResponse() && $exception->getResponse()->getStatusCode() > 400){
-            $message = 
-                json_decode($exception->getResponse()->getBody()->getContents())
+    } catch (GuzzleException $guzzleException) {
+        if ($guzzleException->getResponse() && $guzzleException->getResponse()->getStatusCode() > 400) {
+            $message =
+                json_decode((string) $guzzleException->getResponse()->getBody()->getContents())
                 ->message;
-        }else{
-            $message = $exception->getMessage();
+        } else {
+            $message = $guzzleException->getMessage();
         }
+
         abort($message, 201);
     }
 }
 
-function requestReverseProxy($hostname,$port)
+function requestReverseProxy($hostname, $port)
 {
-    return limanInternalRequest('reverseProxyRequest',[
+    return limanInternalRequest('reverseProxyRequest', [
         "hostname" => $hostname,
         "port" => $port
-    ]); 
+    ]);
 }
 
-function dispatchJob($function_name,$parameters = [])
+function dispatchJob($function_name, $parameters = [])
 {
-    return renderEngineRequest($function_name,"backgroundJob",$parameters);
+    return renderEngineRequest($function_name, "backgroundJob", $parameters);
 }
 
-function renderEngineRequest($function,$url,$parameters = [], $server_id = null, $extension_id = null)
+function renderEngineRequest($function, $url, $parameters = [], $server_id = null, $extension_id = null)
 {
     global $limanData;
-    $client = new Client(['verify' => false ]);
+    $client = new Client(['verify' => false]);
     $parameters["server_id"] = $server_id ? $server_id : server()->id;
     $parameters["extension_id"] = $extension_id ? $extension_id : $limanData["extension"]["id"];
     $parameters["token"] = $limanData["token"];
     $parameters["lmntargetFunction"] = $function;
 
     try {
-        $response = $client->request('POST', "https://127.0.0.1:2806/$url", [
+        $response = $client->request('POST', sprintf('https://127.0.0.1:2806/%s', $url), [
             "form_params" => $parameters,
         ]);
         return $response->getBody()->getContents();
-    } catch (GuzzleException $exception) {
-        abort($exception->getMessage(), 201);
+    } catch (GuzzleException $guzzleException) {
+        abort($guzzleException->getMessage(), 201);
     }
 }
 
 function externalAPI($target, $target_extension_name, $target_server_id, $params = [])
 {
-    return renderEngineRequest($target,"externalAPI",$params, $target_server_id,$target_extension_name);
+    return renderEngineRequest($target, "externalAPI", $params, $target_server_id, $target_extension_name);
 }
 
 // @deprecated
@@ -312,38 +321,39 @@ function publicPath($path)
     return $limanData["publicPath"] . $path;
 }
 
-function getLicense() {
+function getLicense()
+{
     global $limanData;
     return $limanData["license"];
 }
 
 function runCommand($command)
 {
-    return renderEngineRequest('','command',[
+    return renderEngineRequest('', 'command', [
         "command" => $command
     ]);
 }
 
-function runScript($name,$parameters = " ",$sudo = true)
+function runScript($name, $parameters = " ", $sudo = true)
 {
-    return renderEngineRequest('','script',[
-        "local_path" => getPath("scripts/$name"),
+    return renderEngineRequest('', 'script', [
+        "local_path" => getPath(sprintf('scripts/%s', $name)),
         "root" => $sudo ? "yes" : "no",
-        "parameters" => trim($parameters) ? $parameters : " "
+        "parameters" => trim((string) $parameters) !== '' && trim((string) $parameters) !== '0' ? $parameters : " "
     ]);
 }
 
 function putFile($localPath, $remotePath)
 {
-    return renderEngineRequest('','putFile',[
+    return renderEngineRequest('', 'putFile', [
         "local_path" => $localPath,
         "remote_path" => $remotePath,
     ]);
 }
 
-function executeOutsideCommand($connectionType, $username,$password,$remote_host,$remote_port,$command, $disconnect = false)
+function executeOutsideCommand($connectionType, $username, $password, $remote_host, $remote_port, $command, $disconnect = false)
 {
-    return renderEngineRequest('','outsideCommand',[
+    return renderEngineRequest('', 'outsideCommand', [
         "connection_type" => $connectionType,
         "username" => $username,
         "password" => $password,
@@ -371,16 +381,17 @@ function sudo()
 {
     global $limanData;
 
-    if($limanData["key_type"] == "ssh_certificate"){
+    if ($limanData["key_type"] == "ssh_certificate") {
         return "sudo ";
-    } 
+    }
+
     return 'sudo -p "liman-pass-sudo" ';
 }
 
 
 function getFile($localPath, $remotePath)
 {
-    return renderEngineRequest('','getFile',[
+    return renderEngineRequest('', 'getFile', [
         "local_path" => $localPath,
         "remote_path" => $remotePath,
     ]);
@@ -388,7 +399,7 @@ function getFile($localPath, $remotePath)
 
 function openTunnel($remote_host, $socket_port, $username, $password, $ssh_port = 22)
 {
-    return renderEngineRequest('','openTunnel',[
+    return renderEngineRequest('', 'openTunnel', [
         "remote_host" => $remote_host,
         "remote_port" => $socket_port,
         "username" => $username,
@@ -397,9 +408,9 @@ function openTunnel($remote_host, $socket_port, $username, $password, $ssh_port 
     ]);
 }
 
-function keepTunnelAlive($remote_host,$remote_port,$username)
+function keepTunnelAlive($remote_host, $remote_port, $username)
 {
-    return renderEngineRequest('','keepTunnelAlive',[
+    return renderEngineRequest('', 'keepTunnelAlive', [
         "remote_host" => $remote_host,
         "remote_port" => $remote_port,
         "username" => $username
@@ -415,16 +426,16 @@ function stopTunnel($remote_host, $remote_port)
 function getPath($filename = null)
 {
     global $limanData;
-    return dirname(dirname($limanData["functionsPath"])) . "/" . $filename;
+    return dirname(dirname((string) $limanData["functionsPath"])) . "/" . $filename;
 }
 
 function getVariable($key)
 {
     global $limanData;
 
-    if(isset($limanData["variables"]) && isset($limanData["variables"][$key])){
+    if (isset($limanData["variables"]) && isset($limanData["variables"][$key])) {
         return $limanData["variables"][$key];
-    }else{
+    } else {
         return "";
     }
 }
@@ -435,42 +446,44 @@ function can($name)
     if ($limanData["user"]["status"] == "1") {
         return true;
     }
+
     return in_array($name, $limanData["permissions"]);
 }
 
-function sendNotification($title,$message, $type = "notify")
+function sendNotification($title, $message, $type = "notify")
 {
-    return limanInternalRequest('sendNotification',[
+    return limanInternalRequest('sendNotification', [
         "title" => $title,
         "message" => $message,
         "type" => $type
     ]);
 }
 
-function sendMail($to, $subject, $content, array $attachments=[])
+function sendMail($to, $subject, $content, array $attachments = [])
 {
-    return limanInternalRequest('sendMail',[
+    return limanInternalRequest('sendMail', [
         "to" => $to,
         "subject" => $subject,
-        "content" => base64_encode($content),
+        "content" => base64_encode((string) $content),
         "attachments" => json_encode($attachments),
     ]);
 }
 
-function sendLog($title,$message,$data=[])
+function sendLog($title, $message, $data = [])
 {
     global $limanData;
-    if($message == null){
-        abort("Mesaj boş olamaz!",504);
+    if ($message == null) {
+        abort("Mesaj boş olamaz!", 504);
     }
-    if($title == "MAIL_TAG"){
+
+    if ($title == "MAIL_TAG") {
         $message = $limanData["extension"]["id"] . "-" . server()->id . "-" . $message;
     }
 
-    return renderEngineRequest('','sendLog',[
+    return renderEngineRequest('', 'sendLog', [
         "log_id" => $limanData["log_id"],
-        'message' => base64_encode($message),
-        'title' => base64_encode($title),
+        'message' => base64_encode((string) $message),
+        'title' => base64_encode((string) $title),
         'data' => json_encode($data)
     ]);
 }
@@ -478,14 +491,14 @@ function sendLog($title,$message,$data=[])
 function runTask()
 {
     $taskName = request('name');
-    $attributes = (array) json_decode(request('attributes'));
+    $attributes = (array) json_decode((string) request('attributes'));
     return respond(TaskManager::get($taskName, $attributes)->run());
 }
 
 function checkTask()
 {
     $taskName = request('name');
-    $attributes = (array) json_decode(request('attributes'));
+    $attributes = (array) json_decode((string) request('attributes'));
     return respond(TaskManager::get($taskName, $attributes)->check());
 }
 
@@ -495,25 +508,26 @@ if (is_file($limanData["functionsPath"])) {
 
 if (is_file(getPath('vendor/autoload.php'))) {
     require_once getPath('vendor/autoload.php');
-    if(class_exists('App\\App') && method_exists('App\\App', 'init')){
+    if (class_exists('App\\App') && method_exists('App\\App', 'init')) {
         (new App\App())->init();
     }
 }
 
-if(function_exists($limanData["function"])){
+if (function_exists($limanData["function"])) {
     echo call_user_func($limanData["function"]);
-}else if(is_file(getPath("routes.php"))){
+} elseif (is_file(getPath("routes.php"))) {
     $routes = include getPath('routes.php');
-    if(isset($routes[$limanData["function"]])){
-        $destination = explode('@', $routes[$limanData["function"]]);
+    if (isset($routes[$limanData["function"]])) {
+        $destination = explode('@', (string) $routes[$limanData["function"]]);
         $class = 'App\\Controllers\\' . $destination[0];
         if (!class_exists($class)) {
-			$class = $destination[0];
+            $class = $destination[0];
         }
+
         echo (new $class())->{$destination[1]}();
-    }else{
-        abort("İstediğiniz sayfa bulunamadı",504);
+    } else {
+        abort("İstediğiniz sayfa bulunamadı", 504);
     }
-}else{
-    abort("İstediğiniz sayfa bulunamadı",504);
+} else {
+    abort("İstediğiniz sayfa bulunamadı", 504);
 }
