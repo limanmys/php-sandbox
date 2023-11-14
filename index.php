33d989c790059943ece972523d5ed2661eea5571
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-11-14 09:52:01 +0300

    fix: Encoded to array

--- index.php
+++ index.php
@@ -465,7 +465,7 @@ function sendNotification($title, $message, $type = "notify")
 function sendMail($to, $subject, $content, array $attachments = [], bool $templated = false)
 {
     return limanInternalRequest('sendMail', [
-        "to" => $to,
+        "to" => json_encode($to),
         "subject" => $subject,
         "content" => base64_encode((string) $content),
         "attachments" => json_encode($attachments),
